0203fbc0604db82b80bfddea87a288ffc893692f
squonklk <jamipiraino@anthony-piraino.bc.edu>
2016-04-10 21:45:47 -0400

    beginning of project
    added main page and css

--- team_dank.php
+++ team_dank.php
@@ -4,8 +4,17 @@
 <head>
      <meta charset="utf-8" />
      <title>Dankemon</title>
+	 <link rel="stylesheet" type="text/css" href="css/dank.css" />
 </head>
 <body>
- test???
+ <div id="header">
+ <h1>Dankemon</h1>
+ </div>
+ <div id="gamediv"> 
+ 	<p>THIS IS WHERE THE GAME WILL GO</p>
+ </div>
+ <div id="newsdiv">
+ <p>THIS IS WHERE THE NEWS WILL GO</p>
+ </div>
 </body>
 </html>
