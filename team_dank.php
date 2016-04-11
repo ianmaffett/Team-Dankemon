9fff78ef49e507581fcc98eb0446732b1920c293
squonklk <jamipiraino@anthony-piraino.bc.edu>
2016-04-10 21:30:59 -0400

    Merge remote-tracking branch 'origin/tests' into tests
    # Conflicts:
    #	tester2.php

---
+++ team_dank.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+     <meta charset="utf-8" />
+     <title>Dankemon</title>
+</head>
+<body>
+ test???
+</body>
+</html>
---
+++ tester2.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+
+<html lang="en">
+<head>
+     <meta charset="utf-8" />
+     <title>Tester2</title>
+</head>
+<body>
+ <?php 
+  $str = 'This is a test!!';
+  echo $str;
+ ?>
+</body>
+</html>
