37a978f838a41dd4b0c427b7884a8cf7f5a70f8e
squonklk <jamipiraino@anthony-piraino.bc.edu>
2016-04-11 23:53:09 -0400

    updates
    completed pokemon news section and updated css file so that layout
    would work. also created screw around file for code dumps

--- team_dank.php
+++ team_dank.php
@@ -13,8 +13,27 @@
  <div id="gamediv"> 
  	<p>THIS IS WHERE THE GAME WILL GO</p>
  </div>
+ <span class="right">
+ 	<h2>Pokemon News</h2>
+ </span> 
  <div id="newsdiv">
- <p>THIS IS WHERE THE NEWS WILL GO</p>
+ <?php
+ 	function makenews() {
+  		$xml = simplexml_load_file("http://bulbanews.bulbagarden.net/feed/news.rss") or die("Error: Cannot find file");
+  		$str = "";
+  		$i = 0;
+  		$max = $xml->channel->item->count();
+  		for($i = 0 ; $i < $max ; $i++) {
+  			$str .= "<h5><a target='_blank' href='" . $xml->channel->item[$i]->link . "'>" . $xml->channel->item[$i]->title . "</a></h5>";
+  			$str .= "<p>" . $xml->channel->item[$i]->description . "</p>";
+  			$str .= "<p class='small'>Written by " . $xml->channel->item[$i]->author . " and published " . $xml->channel->item[$i]->pubDate . "</p>";
+  		}
+  		$str .= "<p class='small'>Last updated " . $xml->channel->lastBuildDate . " from " . $xml->channel->link . "</p>";
+  		echo $str;
+  	}
+ 	
+ 	makenews();
+ ?>
  </div>
 </body>
 </html>
