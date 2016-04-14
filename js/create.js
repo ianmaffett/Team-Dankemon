40f92eabea0faaae8a99dbdb27c81cd972ee3766
squonklk <jamipiraino@anthony-piraino.bc.edu>
2016-04-13 23:44:05 -0400

    trying to do signup
    could not figure out how to do it with ajax

--- js/create.js
+++ js/create.js
@@ -2,6 +2,16 @@ $(document).ready(function() {
 	$("#creform").hide();
 });
 
+$("#cre").click(function() { 
+	var x = $.ajax({
+		url: "php/create.php",
+		type: "POST"
+	});
+	if (x) {
+		$('#newsdiv').hide()
+	}
+});
+
 $("#create").click(function() {
 	$("#create").hide();
 	$("#forgot").hide();
--- php/create.php
+++ php/create.php
@@ -1,15 +1,16 @@
 <?php 
 
+echo "hey";
+
 $user = $_POST['creusername'];
 $pass = sha1($_POST['crepassword']);
 $play = $_POST['creplayer'];
 $riv = $_POST['crerival'];
+$email = $_POST['creemail'];
 
 $dbc = @mysqli_connect("http://cscilab.bc.edu/phpmyadmin/index.php?db=pirainoa&token=4ed3034051823cd4669b3b6d31334eca", "pirainoa", "47649115", "members") OR die("Could not connect to database: ".mysqli_connect_error());
-$query = "insert into members (username,password,wins,losses,player,rival
+$query = "insert into members (username,password,wins,losses,player,rival,email) values ('$user','$pass',0,0,'$play','$riv','$email')";
 
-if ($_POST['cre']) {
-	
-}
+$result = mysqli_query ($dbc, $query) OR die('Invalid query: ' . mysqli_error($dbc));
 
 ?>
