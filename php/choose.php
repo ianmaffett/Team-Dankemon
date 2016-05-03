66c74d4f37ee81e6ff2d5d9d3968d716f0a22a44
ianmaffett <imaffett1013@yahoo.com>
2016-05-02 21:26:14 -0400

    more stuff

--- php/choose.php
+++ php/choose.php
@@ -2,12 +2,14 @@
 
 <?php
 
-$pokeid = $_GET['pokemonid'];
+$pokeid = $_GET['i'];
 
 $dbc = @mysqli_connect('127.0.0.1', 'pirainoa', 'RZHmZ6qE', 'pirainoa') OR die("Could not connect to database: ".mysqli_connect_error());
 $query = "select name, pokeinfo, pokeimg from pokemon where id='$pokeid'";
 
 $result = mysqli_query ($dbc, $query) OR die('Invalid query: ' . mysqli_error($dbc));
+
+$row = mysqli_fetch_array($result, MYSQLI_ASSOC);
 
 
 echo $result;
