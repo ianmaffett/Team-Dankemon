44063265aedf5466722db7555b51ea00422cb5a5
ianmaffett <imaffett1013@yahoo.com>
2016-05-03 23:36:28 -0400

    CSS Start
    Starting to apply more CSS

--- team_dank.php
+++ team_dank.php
@@ -3,7 +3,7 @@
 <html lang="en">
 <head>
      <meta charset="utf-8" />
-     <title>Dankemon</title>
+     <title>Team Dankemon Project</title>
 	 <link rel="stylesheet" type="text/css" href="css/dank.css" />
 	 <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js"></script>
 	 <script src="js/makenews.js" type="text/javascript"></script>
@@ -12,9 +12,9 @@
  <audio autoplay loop="loop">
 	  <source src="http://66.90.91.26/ost/pokemon-gold-silver/aykljeeyyc/26-pokemon-gym.mp3" type="audio/mpeg">
  </audio>
- 
- <div id="header">
- <h1>Dankemon</h1>
+
+ <div id="header"> <br />
+ <h1><img src="img/pokeball.png" class="bounce" width= 50px height= 50px alt="pokeball"> Dankemon Battle Simulator <img src="img/pokeball.png" width= 50px height= 50px alt="pokeball"></h1> <br /> <br /> <hr /> <br /> <br />
  </div>
  <div id="gamediv">
  	<form name="login" id="login" method="post">
@@ -37,7 +37,7 @@
  	<form name="chooseform" id="chooseform">
  	</form>
  	<button name="reset" id="reset">Play Again</button>
- 	
+
  	<div id='gameleft'>
  		<img src='' alt='Your Pokemon' id="pokeimg" /><br><br>
 		<span id='curhp'></span>/<span id='tothp'></span><br><br>
@@ -47,17 +47,17 @@
 		<input type='radio' name='move' id='move4radio' value='4'><span id='move4'></span><br><br>
 		<button name='choosemove' id='choosemove'>Attack!</button>
 	</div>
-	
+
 	<div id='gameright'>
  		<img src='' alt='Mewtwo' id='mewtwoimg' /><br><br>
 		<span id='opphp'></span>/<span id='totalopphp'></span><br><br>
 	</div>
-	
-	
+
+
  </div>
- <span class="right">
+ <div id="newshead" class="right">
  	<h2>Pokemon News</h2>
- </span>
+ </div>
  <div id="newsdiv">
  </div>
  <div id="newsdiv1">
@@ -67,12 +67,12 @@
   <script src="js/create.js" type="text/javascript"></script>
   <script src="js/valuser.js" type="text/javascript"></script>
   <script src="js/makegame.js" type="text/javascript"></script>
-  <?php 
+  <?php
  	if (isset($_GET['login'])) {
  		echo '<script src="js/choose.js" type="text/javascript"></script>';
  	}
- 
- 	
+
+
  ?>
 </body>
 </html>
